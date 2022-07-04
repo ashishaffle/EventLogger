eff3812d8f463fb11ce0a9e956f42333e1425776
Ashish <ashish@MacBook-Pro.local>
2022-07-04 16:36:54 +0530

    initital code commit

---
+++ VizuryEventLogger/build/VizuryEventLogger.build/Debug-iphonesimulator/VizuryEventLogger.build/Objects-normal/x86_64/VizuryEventLogger_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/ashish/Documents/bitbucket/ios-sdk/VizuryEventLogger/build/VizuryEventLogger.build/Debug-iphonesimulator/VizuryEventLogger.build/DerivedSources/VizuryEventLogger_vers.c
